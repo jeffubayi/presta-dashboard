a329dccdd5f333175569ada29a77f76c8b2ac6a3
jeffubayi <jeffubayi@gmail.com>
2023-06-03 14:31:14 +0300

    UI -add filters,search,pagination inputs

--- src/views/CustomerView.vue
+++ src/views/CustomerView.vue
@@ -11,6 +11,7 @@ import {
   Dropdown,
   ListGroup,
   ListGroupItem,
+  Toast,
 } from "flowbite-vue";
 import { ref, toRefs, onMounted } from "vue";
 
@@ -29,6 +30,9 @@ const loan = ref();
 const approved = ref();
 const branch = ref("");
 const email = ref("");
+const searchItem = ref("");
+const filteredSalesRep = ref("");
+const filteredBranch = ref("");
 
 console.log(`session data`, session);
 
@@ -47,7 +51,9 @@ function showEditModal(payload: any) {
 }
 
 onMounted(() => {
+  // window.setInterval(() => {
   getAllCustomers();
+  // }, 3000);
 });
 
 async function getAllCustomers() {
@@ -56,7 +62,8 @@ async function getAllCustomers() {
 
     const { data, error, status } = await supabase
       .from("customers")
-      .select(`*`);
+      .select(`*`)
+      .limit(6);
 
     if (error && status !== 406) throw error;
     if (data) {
@@ -66,6 +73,59 @@ async function getAllCustomers() {
     console.log(error);
   } finally {
     loading.value = false;
+  }
+}
+
+async function search() {
+  try {
+    loading.value = true;
+
+    const { data, error } = await supabase
+      .from("customers")
+      .select(`*`)
+      .like("name", searchItem.value);
+
+    if (error) throw error;
+    if (data) {
+      customers.value = data;
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+async function filterByBranch() {
+  console.log(`daaa`, filteredBranch.value);
+  try {
+    const { data, error } = await supabase
+      .from("customers")
+      .select(`*`)
+      .like("branch", filteredBranch.value);
+
+    if (error) throw error;
+    if (data) {
+      customers.value = data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function filterBySales() {
+  try {
+    const { data, error } = await supabase
+      .from("customers")
+      .select(`*`)
+      .like("branch", filteredSalesRep.value);
+
+    if (error) throw error;
+    if (data) {
+      customers.value = data;
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -145,7 +205,6 @@ async function signOut() {
     loading.value = false;
   }
 }
-
 </script>
 
 <template>
@@ -166,6 +225,7 @@ async function signOut() {
         <NavbarLink>Loans</NavbarLink>
       </NavbarCollapse>
     </template>
+
     <template #right-side>
       <div class="flex items-center space-x-4 mx-1">
         <img
@@ -186,7 +246,11 @@ async function signOut() {
       </Button>
     </template>
   </Navbar>
-
+  <!-- <template>
+    <Toast v-if="isShowModal" type="success">
+      Item moved successfully.
+    </Toast>
+  </template> -->
   <Modal size="md" v-if="isEditModal" @close="closeEditModal">
     <template #header>
       <p>Edit Customer {{ selectedCustomers.name }}</p>
@@ -208,7 +272,7 @@ async function signOut() {
             v-model="phone"
             required
             size="sm"
-            :value="selectedCustomers.phone"
+            type="tel"
           />
         </div>
         <div>
@@ -217,6 +281,7 @@ async function signOut() {
             v-model="email"
             required
             size="sm"
+            type="email"
             :value="selectedCustomers.email"
           />
         </div>
@@ -244,6 +309,7 @@ async function signOut() {
             label="Loan"
             v-model="loan"
             required
+            type="number"
             :value="selectedCustomers.loan"
             size="sm"
           />
@@ -256,13 +322,14 @@ async function signOut() {
                 v-model="approved"
                 required
                 :value="selectedCustomers.approved"
+                type="number"
                 size="sm"
               />
             </div>
           </div>
           <div class="flex items-start">
             <div class="flex items-center h-5">
-              <input
+              <Input
                 id="remember"
                 type="checkbox"
                 :value="selectedCustomers.used"
@@ -301,95 +368,93 @@ async function signOut() {
 
   <Modal size="md" v-if="isShowModal" @close="closeModal">
     <template #header>
-      <p>New Customer </p>
+      <p>New Customer</p>
     </template>
     <template #body>
-          <form class="space-y-4" is-form @submit.prevent="addCustomer">
-            <div>
-              <Input label="Name" v-model="name" required size="sm" />
-            </div>
-            <div>
+      <form class="space-y-4" is-form @submit.prevent="addCustomer">
+        <div>
+          <Input label="Name" v-model="name" required size="sm" />
+        </div>
+        <div>
+          <Input
+            label="Phone Number"
+            v-model="phone"
+            required
+            size="sm"
+            type="tel"
+          />
+        </div>
+        <div>
+          <Input
+            label="Email Address"
+            v-model="email"
+            required
+            size="sm"
+            type="email"
+          />
+        </div>
+        <div>
+          <label
+            for="countries"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Branch</label
+          >
+          <select
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Choose a branch</option>
+            <option value="Nairobi">Nairobi</option>
+            <option value="Kisumu">Kisumu</option>
+            <option value="Mombasa">HQ</option>
+          </select>
+        </div>
+        <div>
+          <label
+            for="countries"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Sales Rep</label
+          >
+          <select
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Choose a sales Rep</option>
+            <option value="Jane Wahu">Jane Wahu</option>
+            <option value="Tom Ogola">Tom Ogola</option>
+            <option value="Sam Okoye">Sam Okoye</option>
+          </select>
+        </div>
+        <div>
+          <Input label="Loan" v-model="loan" required type="number" size="sm" />
+        </div>
+        <div class="flex justify-between">
+          <div class="flex items-start">
+            <div class="flex items-center h-5 mt-2 mb-1">
               <Input
-                label="Phone Number"
-                v-model="phone"
+                label="Approved"
+                v-model="approved"
                 required
+                type="number"
                 size="sm"
               />
             </div>
-            <div>
+          </div>
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
               <Input
-                label="Email Address"
-                v-model="email"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                 required
-                size="sm"
               />
             </div>
-            <div>
-              <label
-                for="countries"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Branch</label
-              >
-              <select
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option selected>Choose a branch</option>
-                <option value="Nairobi">Nairobi</option>
-                <option value="Kisumu">Kisumu</option>
-                <option value="Mombasa">HQ</option>
-              </select>
-            </div>
-            <div>
-              <label
-                for="countries"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Sales Rep</label
-              >
-              <select
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option selected>Choose a sales Rep</option>
-                <option value="Jane Wahu">Jane Wahu</option>
-                <option value="Tom Ogola">Tom Ogola</option>
-                <option value="Sam Okoye">Sam Okoye</option>
-              </select>
-            </div>
-            <div>
-              <Input
-                label="Loan"
-                v-model="loan"
-                required
-                size="sm"
-              />
-            </div>
-            <div class="flex justify-between">
-              <div class="flex items-start">
-                <div class="flex items-center h-5 mt-2 mb-1">
-                  <Input
-                    label="Approved"
-                    v-model="approved"
-                    required
-                    size="sm"
-                  />
-                </div>
-              </div>
-              <div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input
-                    type="checkbox"
-                    value=""
-                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  for="remember"
-                  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >Used</label
-                >
-              </div>
-            </div>
-          </form>
+            <label
+              for="remember"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Used</label
+            >
+          </div>
+        </div>
+      </form>
     </template>
     <template #footer>
       <div class="flex justify-evenly gap-2">
@@ -436,18 +501,72 @@ async function signOut() {
         </div>
         <form>
           <div class="flex justify-between">
-            <dropdown text="Filter by">
-              <list-group>
-                <list-group-item> Label </list-group-item>
-                <list-group-item> Branch </list-group-item>
-                <list-group-item> Sales Rep </list-group-item>
-              </list-group>
-            </dropdown>
-            <Input size="sm" placeholder="Search for...">
-              <template #prefix>
+            <div class="flex justify-start gap-2">
+              <form
+                class="flex items-center"
+                is-form
+                @submit.prevent="filterByBranch"
+              >
+                <select
+                  id="branch"
+                  v-model="filteredBranch"
+                  v-on:input="filterByBranch"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option selected>Select Branch</option>
+                  <option value="HQ">HQ</option>
+                  <option value="Mombasa">Mombasa</option>
+                  <option value="Kisumu">Kisumu</option>
+                </select>
+              </form>
+              <form class="flex items-center">
+                <select
+                  id="sales"
+                  v-bind:value="filteredSalesRep"
+                  v-on:input="filterBySales"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option selected>Select Sales Rep</option>
+                  <option value="Jane Wahu">Jane Wahu</option>
+                  <option value="Tom Ogola">Tom Ogol</option>
+                </select>
+              </form>
+            </div>
+            <form class="flex items-center" is-form @submit.prevent="search">
+              <div class="relative w-full">
+                <div
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+                <input
+                  size="sm"
+                  v-model="searchItem"
+                  type="text"
+                  id="simple-search"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search for..."
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
                 <svg
-                  aria-hidden="true"
-                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  class="w-5 h-5"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -460,8 +579,9 @@ async function signOut() {
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                   ></path>
                 </svg>
-              </template>
-            </Input>
+                <span class="sr-only">Search</span>
+              </button>
+            </form>
           </div>
         </form>
       </caption>
@@ -533,23 +653,71 @@ async function signOut() {
               >Yes</span
             >
           </td>
-          <td class="px-6 py-4 text-right">
-            <Button size="sm" outline @click="showEditModal(customer)">
-              Edit
-            </Button>
+          <td class="px-4 py-4 text-right">
+            <Button size="sm" @click="showEditModal(customer)"> Edit </Button>
           </td>
-          <td>
-            <Button
-              size="sm"
-              outline
-              color="red"
-              @click="deleteCustomer(customer)"
-            >
-              Delete
+          <td class="px-4 py-4">
+            <Button size="sm" color="red" @click="deleteCustomer(customer)">
+              <svg
+                class="w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
             </Button>
           </td>
         </tr>
       </tbody>
     </table>
+  </div>
+  <div
+    class="sticky bottom-0 right-0 items-center w-full p-4 bg-white border-t border-gray-200 sm:flex sm:justify-between dark:bg-gray-800 dark:border-gray-700"
+  >
+    <div class="flex items-center mb-4 sm:mb-0">
+      <span class="text-sm font-normal text-gray-500 dark:text-gray-400"
+        >Showing
+        <span class="font-semibold text-gray-900 dark:text-white">1-6</span> of
+        <span class="font-semibold text-gray-900 dark:text-white"
+          >20</span
+        ></span
+      >
+    </div>
+    <div class="flex items-center space-x-3">
+      <Button color="dark" outline square>
+        <svg
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </Button>
+
+      <Button color="dark" outline square>
+        <svg
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </Button>
+    </div>
   </div>
 </template>
