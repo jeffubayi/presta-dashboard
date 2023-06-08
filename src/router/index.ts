ef00ffa20e63e19abc1c71d2c72b0b541133ccdd
jeffubayi <jeffubayi@gmail.com>
2023-06-08 18:06:00 +0300

    add routes

---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ProfileView from "../views/ProfileView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfileView,
+    },
+    {
+      path: "/customers",
+      name: "customers",
+      component: () => import("../views/CustomerView.vue"),
+    },
+  ],
+});
+
+export default router;
