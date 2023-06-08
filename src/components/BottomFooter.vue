2614b4e34ecc913e15bf2b5b50b8c13ae1397886
jeffubayi <jeffubayi@gmail.com>
2023-06-08 18:06:27 +0300

    update navbar ui

---
+++ src/components/BottomFooter.vue
@@ -0,0 +1,348 @@
+<template>
+  <div class="container px-4 pt-5 mx-auto md:pt-10 lg:px-0 dark:bg-gray-900">
+    <h1
+      class="mb-3 text-3xl font-bold text-gray-900 sm:text-4xl sm:leading-none sm:tracking-tight dark:text-white"
+    >
+      Our loan plan made simple
+    </h1>
+    <p
+      class="mb-6 text-lg font-normal text-gray-500 sm:text-xl dark:text-gray-400"
+    >
+      All types of businesses need access to development resources, so we give
+      you the option to decide how much you need to use.
+    </p>
+
+    <!-- Pricing Cards -->
+    <section
+      class="grid grid-cols-1 space-y-12 md:space-y-0 md:grid-cols-2 lg:grid-cols-3 md:gap-x-6 md:gap-6 pt-9"
+    >
+      <!-- Pricing Card -->
+      <div
+        class="flex flex-col max-w-lg p-6 mx-auto text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 xl:p-8 dark:bg-gray-800 dark:text-white"
+      >
+        <h3 class="mb-4 text-2xl font-semibold">Starter</h3>
+        <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+          Best option for personal use & for your next project.
+        </p>
+        <div class="flex items-baseline my-8">
+          <span class="mr-2 text-5xl font-extrabold">$29</span>
+          <span class="text-gray-500 dark:text-gray-400">/month</span>
+        </div>
+        <!-- List -->
+        <ul role="list" class="mb-8 space-y-4 text-left">
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>Individual configuration</span>
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>No setup, or hidden fees</span>
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Team size: <span class="font-semibold">1 developer</span></span
+            >
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Premium support:
+              <span class="font-semibold">6 months</span></span
+            >
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Free updates: <span class="font-semibold">6 months</span></span
+            >
+          </li>
+        </ul>
+        <a
+          href="#"
+          class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-primary-900"
+          >Get started</a
+        >
+      </div>
+      <!-- Pricing Card -->
+      <div
+        class="flex flex-col max-w-lg p-6 mx-auto text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 xl:p-8 dark:bg-gray-800 dark:text-white"
+      >
+        <h3 class="mb-4 text-2xl font-semibold">Company</h3>
+        <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+          Relevant for multiple users, extended & premium support.
+        </p>
+        <div class="flex items-baseline my-8">
+          <span class="mr-2 text-5xl font-extrabold">$99</span>
+          <span class="text-gray-500 dark:text-gray-400" dark:text-gray-400
+            >/month</span
+          >
+        </div>
+        <!-- List -->
+        <ul role="list" class="mb-8 space-y-4 text-left">
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>Individual configuration</span>
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>No setup, or hidden fees</span>
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Team size: <span class="font-semibold">10 developers</span></span
+            >
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Premium support:
+              <span class="font-semibold">24 months</span></span
+            >
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Free updates: <span class="font-semibold">24 months</span></span
+            >
+          </li>
+        </ul>
+        <a
+          href="#"
+          class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-primary-900"
+          >Get started</a
+        >
+      </div>
+      <!-- Pricing Card -->
+      <div
+        class="flex flex-col max-w-lg p-6 mx-auto text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 xl:p-8 dark:bg-gray-800 dark:text-white"
+      >
+        <h3 class="mb-4 text-2xl font-semibold">Enterprise</h3>
+        <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+          Best for large scale uses and extended redistribution rights.
+        </p>
+        <div class="flex items-baseline my-8">
+          <span class="mr-2 text-5xl font-extrabold">$499</span>
+          <span class="text-gray-500 dark:text-gray-400">/month</span>
+        </div>
+        <!-- List -->
+        <ul role="list" class="mb-8 space-y-4 text-left">
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>Individual configuration</span>
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span>No setup, or hidden fees</span>
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Team size:
+              <span class="font-semibold">100+ developers</span></span
+            >
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Premium support:
+              <span class="font-semibold">36 months</span></span
+            >
+          </li>
+          <li class="flex items-center space-x-3">
+            <!-- Icon -->
+            <svg
+              class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              >Free updates: <span class="font-semibold">36 months</span></span
+            >
+          </li>
+        </ul>
+        <a
+          href="#"
+          class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-primary-900"
+          >Get started</a
+        >
+      </div>
+    </section>
+  </div>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import {
+  Button,
+  Navbar,
+  NavbarLogo,
+  NavbarCollapse,
+  NavbarLink,
+} from "flowbite-vue";
+import { onMounted, ref, computed, reactive } from "vue";
+import router from "@/router";
+
+import { supabase } from "../supabase";
+import { useMainStore } from "@/stores/main";
+
+const mainStore = useMainStore();
+const session = ref();
+const route = useRoute();
+const loading = ref<boolean>(true);
+const profile = reactive({
+  name: mainStore.userName,
+  email: mainStore.userEmail,
+});
+
+const currentRouteName = computed(() => route.name);
+console.log(`route`, currentRouteName);
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session;
+  });
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session;
+  });
+});
+//log out user
+async function signOut() {
+  try {
+    loading.value = true;
+    let { error } = await supabase.auth.signOut();
+    if (error) throw error;
+    router.push("/");
+  } catch (error) {
+    alert(error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+const signUp = async () => {
+  try {
+    loading.value = true;
+    const { error } = await supabase.auth.signInWithPassword({
+      email: "kibe@presta.co.ke",
+      password: "12345678",
+    });
+    if (error) throw error;
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message);
+    }
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<template>
+  <Navbar>
+    <template #logo>
+      <NavbarLogo
+        link="/"
+        alt="Presta"
+        image-url="https://www.presta.co.ke/assets/logo-7ca6d1fb.svg"
+      >
+        Presta Capital
+      </NavbarLogo>
+    </template>
+    <template v-if="session" #default="{ isShowMenu }">
+      <NavbarCollapse :isShowMenu="isShowMenu">
+        <NavbarLink v-if="currentRouteName" link="/">Home</NavbarLink>
+        <NavbarLink is-active link="/customers">Customers</NavbarLink>
+        <NavbarLink link="/profile">Profile</NavbarLink>
+      </NavbarCollapse>
+    </template>
+
+    <template #right-side>
+      <div v-if="session" class="flex items-center space-x-2 mx-2">
+        <img
+          class="w-9 h-9 rounded-full"
+          src="https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png"
+          alt=""
+        />
+        <div class="font-medium dark:text-white">
+          <div>{{ profile.name }}</div>
+          <div class="text-sm text-gray-500 dark:text-gray-400">
+            {{ profile.email || "Not signed in" }}
+          </div>
+        </div>
+      </div>
+      <Button v-if="session" size="xs" @click="signOut"> Sign out </Button>
+      <Button v-else size="md" @click="signUp"> Get started</Button>
+    </template>
+  </Navbar>
+</template>
