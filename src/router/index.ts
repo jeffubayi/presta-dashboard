e5e9cc8367aa6d5f711413655daa432a4cb73311
jeffubayi <jeffubayi@gmail.com>
2023-06-09 09:12:20 +0300

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
