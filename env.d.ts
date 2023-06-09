3d01f9ff4764d548c93431362becc46f7fcedb1e
jeffubayi <jeffubayi@gmail.com>
2023-06-09 08:17:59 +0300

    update styles

--- env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string = "https://elzmklegnujpwsdcepdx.supabase.co";
-  readonly VITE_SUPABASE_ANON_KEY: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsem1rbGVnbnVqcHdzZGNlcGR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU1OTc2NTAsImV4cCI6MjAwMTE3MzY1MH0.cnotF6pkylhY4Y3WViqDBFEHP21SnvXvVXeHdDOJfCQ";
-}
